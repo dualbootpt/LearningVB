5eba35ced2c3e89f8712bc5cc3b4c85012406940
Paulo Alexandre Romualdo <paulo.romualdo@dualboot.pt>
2012-07-08 21:32:14 +0100

    First Commit.
    Learnig VB

---
+++ Hello World/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Close()
+
+    End Sub
+End Class
