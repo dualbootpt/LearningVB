a1b92047f97bc7b1db6686b47cea8be25b7cdd05
Paulo Alexandre Romualdo <paulo.romualdo@dualboot.pt>
2012-07-12 00:39:04 +0100

    Thread experiment Updating external gui objects from the thread using a function delegate

--- Hello World/Form1.Designer.vb
+++ Hello World/Form1.Designer.vb
@@ -24,6 +24,8 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -39,12 +41,30 @@ Partial Class Form1
         'Button1
         '
         Me.Button1.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Button1.Location = New System.Drawing.Point(52, 155)
+        Me.Button1.Location = New System.Drawing.Point(52, 93)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(188, 23)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "O.K."
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(52, 215)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Start"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 169)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
         '
         'Form1
         '
@@ -52,6 +72,8 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.Button1
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -65,5 +87,7 @@ Partial Class Form1
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
--- Hello World/Form1.vb
+++ Hello World/Form1.vb
@@ -1,6 +1,23 @@
-﻿Public Class Form1
+﻿Imports System.Threading
+
+Public Class Form1
+
+    Delegate Sub updateCounterDelegate(ByVal newValue As Integer)
 
     Dim msg As String = "Hello World"
+
+    Private Sub updateCounter(ByVal newValue As Integer)
+        Label2.Text = newValue
+        Label2.Refresh()
+    End Sub
+
+    Public Sub thread1()
+        Dim updateCounterCallback As New updateCounterDelegate(AddressOf updateCounter)
+        For i As Integer = 0 To 100
+            Invoke(updateCounterCallback, i)
+            Thread.Sleep(20)
+        Next
+    End Sub
 
     Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
         Label1.Text = msg
@@ -9,5 +26,10 @@
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Close()
 
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim t1 As New Thread(AddressOf thread1)
+        t1.Start()
     End Sub
 End Class
