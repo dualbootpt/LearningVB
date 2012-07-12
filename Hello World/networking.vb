a6d515da6616f600d1a603caf2ae0cfe57039099
Paulo Alexandre Romualdo <paulo.romualdo@dualboot.pt>
2012-07-12 23:58:57 +0100

    multithreaded tct listener added new files

---
+++ Hello World/networking.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.IO
+Imports System.Text
+Imports System.Threading
+
+Module networking
+
+    Public stpd As Boolean = True
+
+    Public Sub startSyncServer()
+        Dim soc As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+        Dim ipAddr As IPAddress = IPAddress.Parse("127.0.0.1")
+        Dim localAddr As New IPEndPoint(ipAddr, 11000)
+        Dim conn As Socket
+        Dim thrd As Thread
+
+        soc.Bind(localAddr)
+        soc.Listen(10)
+
+        While Not stpd
+            conn = soc.Accept()
+            thrd = New Thread(AddressOf worker)
+            thrd.Start(conn)
+        End While
+
+        soc.Close()
+    End Sub
+
+    Private Sub worker(ByVal s As Socket)
+        Dim msg As Byte() = Encoding.UTF8.GetBytes("ConnectionState established")
+        Thread.Sleep(1000)
+        s.Send(msg)
+
+        Thread.Sleep(10000)
+        s.Shutdown(SocketShutdown.Both)
+        s.Close()
+    End Sub
+End Module
